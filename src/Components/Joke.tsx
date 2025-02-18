71eb8e5a6f3cdf6cb1873e227071303222f56bc4
Anri Akopian <anri-akk@mail.ru>
2025-02-18 22:46:36 +0400

    Added a little of styling

--- src/Components/Joke.tsx
+++ src/Components/Joke.tsx
@@ -46,19 +46,24 @@ export default function Joke() {
     if (isLoading) return <p>Loading...</p>;
     if (isError) return <p>Failed to load joke</p>;
     return (
-        <div>
-            <div id={`${displayedJoke._id}`}>
-                <h2>
+        <div className="max-w-3xl mx-auto px-4">
+            <div
+                id={`${displayedJoke._id}`}
+                className="p-6 bg-background text-foreground rounded-xl shadow-lg max-w-lg mx-auto mt-6"
+            >
+                <h2 className="text-2xl font-bold mb-4 text-center">
                     {isFetchingNewJoke ? 'Loading...' : displayedJoke.question}
                 </h2>
-                <p>
+
+                <p className="text-lg text-center text-gray-700">
                     {isFetchingNewJoke
                         ? 'Please wait...'
                         : displayedJoke.answer}
                 </p>
             </div>
+
             {displayedJoke.votes.length > 0 && (
-                <div className="flex gap-2 mt-2">
+                <div className="flex gap-4 justify-center mt-6">
                     {displayedJoke.votes.map((vote) => (
                         <LabelButton
                             key={vote.label}
@@ -70,10 +75,13 @@ export default function Joke() {
                     ))}
                 </div>
             )}
-            <NextJokeButton
-                handleNextJoke={handleNextJoke}
-                isFetching={isFetchingNewJoke}
-            />
+
+            <div className="mt-8 flex justify-center">
+                <NextJokeButton
+                    handleNextJoke={handleNextJoke}
+                    isFetching={isFetchingNewJoke}
+                />
+            </div>
         </div>
     );
 }
--- src/Components/NextJokeButton.tsx
+++ src/Components/NextJokeButton.tsx
@@ -8,8 +8,12 @@ export default function NextJokeButton({
     isFetching,
 }: NextJokeButtonProps) {
     return (
-        <button onClick={handleNextJoke} disabled={isFetching}>
-            {isFetching ? "Loading..." : "Next Joke"}
+        <button
+            onClick={handleNextJoke}
+            disabled={isFetching}
+            className="bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600 transition duration-200"
+        >
+            {isFetching ? 'Loading...' : 'Next Joke'}
         </button>
     );
 }
